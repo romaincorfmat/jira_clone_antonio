77e28c5829694dc92b72598af5b6e1d6c5139358
Romain <romain.corfmatpro@gmail.com>
2024-11-10 13:20:18 +0800

    implement member deletion and promotion

--- src/features/workspaces/components/workspace-avatar.tsx
+++ src/features/workspaces/components/workspace-avatar.tsx
@@ -1,43 +1,42 @@
+import Image from "next/image";
+import React from "react";
+
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { cn } from "@/lib/utils";
 
-import Image from "next/image";
-
 interface WorkspaceAvatarProps {
-	image?: string;
-	name: string;
-	className?: string;
+  image?: string;
+  name: string;
+  className?: string;
 }
 
-import React from "react";
-
 export const WorkspaceAvatar = ({
-	image,
-	name,
-	className,
+  image,
+  name,
+  className,
 }: WorkspaceAvatarProps) => {
-	if (image) {
-		return (
-			<div
-				className={cn(
-					"size-10 relative rounded-md overflow-hidden",
-					className,
-				)}>
-				<Image
-					src={image}
-					alt={name}
-					fill
-					className="object-cover"
-				/>
-			</div>
-		);
-	}
+  if (image) {
+    return (
+      <div
+        className={cn(
+          "size-10 relative rounded-md overflow-hidden",
+          className
+        )}>
+        <Image
+          src={image}
+          alt={name}
+          fill
+          className="object-cover"
+        />
+      </div>
+    );
+  }
 
-	return (
-		<Avatar className={cn("size-10 rounded-md", className)}>
-			<AvatarFallback className="text-white rounded-md bg-blue-600 font-semibold text-lg uppercase">
-				{name[0]}
-			</AvatarFallback>
-		</Avatar>
-	);
+  return (
+    <Avatar className={cn("size-10 rounded-md", className)}>
+      <AvatarFallback className="rounded-md bg-blue-600 text-lg font-semibold uppercase text-white">
+        {name[0]}
+      </AvatarFallback>
+    </Avatar>
+  );
 };
